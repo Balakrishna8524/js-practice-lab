5e24c677f65fc228c941175b1dddf9eddc29b9cb
Balakrishna8524 <balakrishna8524@gmail.com>
2024-07-27 07:34:00 +0530

    Update: js array flatten challenge edits.

--- questions/array/flattenArray.js
+++ questions/array/flattenArray.js
@@ -1,4 +1,25 @@
-// Function to flatten an array
+
+
+
+// using built in function =====================================================================
+/*
+Explanation:
+
+arr.flat(Infinity):
+ - arr refers to the input array.
+ - .flat() is a built-in JavaScript array method. It's used to create a new array with all sub-array elements 
+    concatenated into it recursively up to a specified depth.
+ - The argument passed to .flat() determines how deep the flattening will go.
+ - Infinity is a numeric value representing positive infinity. When you pass Infinity to .flat(), it ensures that all 
+    nested arrays, no matter how deep, are flattened into a single-level array.
+*/
+function flattenArr(arr) {
+  return arr.flat(Infinity);
+}
+
+
+
+// Function to flatten an array  =====================================================================
 function flattenArray(arr) {
   const result = [];
 
@@ -15,4 +36,7 @@ function flattenArray(arr) {
   return result;
 }
 
-module.exports = flattenArray;
+
+
+// Example
+console.log(flattenArr([1, [2, [3, [4]]]])); // [1, 2, 3, 4]
